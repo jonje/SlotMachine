5c6c193625bf95ec8be940927ab4fc739809de56
Jake Wedemeyer <selslurjake1993@gmail.com>
2013-11-27 16:01:05 -0700

    Finished CoinAcceptor

--- src/edu/neumont/lab/CoinAcceptor.java
+++ src/edu/neumont/lab/CoinAcceptor.java
@@ -1,5 +1,60 @@
 package edu.neumont.lab;
 
+import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+
 public class CoinAcceptor {
+	private final int COIN_LIGHT = 40;
+	private LightSensor l;
+	private TouchSensor t;
+	
+	/*
+	Create
+		Initialize LightSensor
+		Initialize TouchSensor
+	*/
+	public CoinAcceptor()
+	{
+		//Initialize LightSensor
+		l = new LightSensor(SensorPort.S1);
+		//Initialize TouchSensor
+		t = new TouchSensor(SensorPort.S2);
+	}
+	
+
+	/*
+	AcceptCoin
+		wait for TouchSensor to be pressed
+		open coin gate with static class Motor
+		use LightSensor to see if a coin has been entered
+		close coin gate
+		return true or false based on the above 
+	*/
+	public boolean AcceptCoin()
+	{
+		boolean didCoinPass = false;
+		
+		//wait for TouchSensor to be pressed
+		while(!t.isPressed());
+		//open coin gate with static class Motor
+		System.out.println(Motor.A.getSpeed());
+		Motor.A.rotate(-60);
+		//use LightSensor to see if a coin has been entered
+		for(int i = 0; i < 100; i++)
+		{
+			if(l.getLightValue() > COIN_LIGHT)
+			{
+				didCoinPass = true;
+				i = 100;
+			}
+		}
+		//Close coin gate
+		Motor.A.rotate(60);
+		
+		//Return true or false based on the above
+		return didCoinPass;
+	}
 
 }
