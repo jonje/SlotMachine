535f081bec03f139f9f934a89ccab6971d5b2a73
Jake Wedemeyer <selslurjake1993@gmail.com>
2013-11-27 16:35:28 -0700

    SmRunner fixed

--- src/edu/neumont/lab/SmRunner.java
+++ src/edu/neumont/lab/SmRunner.java
@@ -3,7 +3,8 @@ package edu.neumont.lab;
 public class SmRunner {
 
 	public static void main(String[] args) {
-		System.out.println("this works");
+		SlotMachine s = new SlotMachine();
+		s.run();
 
 	}
 
